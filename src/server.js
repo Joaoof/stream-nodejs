60a0c69912d75d1862953382858a46e7efc4e2d9
Joaoof <joaodeus400@gmail.com>
2023-02-22 16:26:04 -0300

    generate function

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import http from 'node:http'
 import { Readable } from 'node:stream'
+import { randomUUID } from 'node:crypto'
+
+function * run () {
+  for(let index = 0; index <= 99; index++) {
+    const data = {
+      id: randomUUID(),
+      name: `João-${index}`
+    }
+      yield data
+  }
+}
 
 async function handler(req, res) {
   const readable = new Readable({
     read() {
-
+      for(const data of run()) {
+        console.log('sending', data)
+        this.push(JSON.stringify(data) + "\n")
+      }
       // para informar que os dados acabaram
       this.push(null)
     }
